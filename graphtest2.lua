83cfc7c3dda8b3be2aca8f0ec69ead39115b9e99
groverburger <zachary.m.booth@gmail.com>
2021-01-31 14:02:15 -0800

    new graph system

--- graphtest2.lua
+++ graphtest2.lua
@@ -11,12 +11,28 @@ local people = {
 math.randomseed(os.time())
 
 local lume = require "lume"
-people = lume.shuffle(people)
-for i,v in ipairs(people) do
-    print(i,v)
-end
+
+local murderer
 
 local function makeGraph()
+    people = lume.shuffle(people)
+
+    -- if there is already a murderer defined
+    -- then put the murderer at the end
+    if murderer then
+        local i = 1
+        while i < #people do
+            if people[i] == murderer then
+                table.remove(people, i)
+                table.insert(people, murderer)
+            else
+                i = i + 1
+            end
+        end
+    end
+
+    -- split the people list into random
+    -- strongly connected components
     local graph = {}
     local component = {}
     local interval = math.random(2)
@@ -37,11 +53,11 @@ local function makeGraph()
     end
     print("-------")
 
-    local murderer
-
-    -- get rid of self-references
     for person, list in pairs(graph) do
         local count = 0
+
+        -- get rid of self-references
+        -- and count the amount of references
         for i, other in pairs(list) do
             if other == person then
                 list[i] = nil
@@ -49,11 +65,11 @@ local function makeGraph()
                 count = count + 1
             end
         end
-        --print(person, count)
 
         if count == 0 then
-            murderer = person
-            --print("murderer: " .. murderer)
+            if not murderer then
+                murderer = person
+            end
 
             local r = math.random(#people)
             local newRef = people[r]
@@ -71,54 +87,33 @@ local function makeGraph()
         end
     end
 
-    return graph, murderer
+    return graph
 end
 
 local function makeSus(graphList)
-    local murderer = people[math.random(#people)]
-    local hasMadeSus = false
-    print("the murderer is " .. murderer)
+    local sussed = {}
 
-    for i,graph in ipairs(graphList) do
+    for _,graph in ipairs(graphList) do
         for name,list in pairs(graph) do
-            if list[murderer] then
-                -- count the number of people in this list
-                -- if we have more than one person, the sus can be straight up removed
+            if not sussed[name] and math.random() < 0.25 then
                 local count = 0
-                for _,_ in pairs(list) do
+                local nameToRemove
+                for n,_ in pairs(list) do
                     count = count + 1
+                    nameToRemove = n
                 end
 
-                -- only make this person sus if we have to
-                -- or a random chance
-                if not hasMadeSus or math.random() > 0.5 then
-                    list[murderer] = nil
-
-                    -- if this person has no other connections
-                    -- or by random chance
-                    -- then change their connection instead of straight up removing it
-                    if count == 1 or math.random() > 0.5 then
-                        local r = math.random(#people)
-                        local newRef = people[r]
-
-                        -- don't choose either the murderer or self
-                        while newRef == murderer or newRef == name do
-                            r = r + 1
-                            if r > #people then
-                                r = 1
-                            end
-                            newRef = people[r]
-                        end
-
-                        list[newRef] = newRef
-                        hasMadeSus = true
-                    end
+                if count > 1 then
+                    sussed[name] = true
+                    list[nameToRemove] = nil
                 end
             end
         end
     end
 
-    return murderer
+    for i,v in pairs(sussed) do
+        print("sussed " .. i)
+    end
 end
 
 local function printGraph(graphList)
@@ -130,11 +125,22 @@ local function printGraph(graphList)
             end
             print(str)
         end
+        --print("-------------")
     end
 end
 
+--print("-------")
 local graphList = {makeGraph(), makeGraph()}
 
+--[[
+printGraph(graphList)
+print("-------------")
+makeSus(graphList)
+print("-------------")
+printGraph(graphList)
+]]
+
+--[[
 for name,connectionList in pairs(graphList[1]) do
     local str = name .. ": "
     for _,connection in pairs(connectionList) do
@@ -150,4 +156,5 @@ for name,connectionList in pairs(graphList[2]) do
     end
     print(str)
 end
+]]
 --printGraph(graphList)
